81f5823c9c56f93e132e0f2a03ab395821a4d505
Thomas Lecointre <thomas.lecointre1@gmail.com>
2017-05-09 12:24:46 +0200

    Auto stash before merge of "develop" and "origin/develop"

---
+++ server/server.js
@@ -0,0 +1,21 @@
+// Imports
+var express = require("express");
+var fs      = require("fs");
+var request = require("request");
+var cheerio = require("cheerio");
+var app     = express();
+var imdb    = require("imdb-api");
+
+// Transform client requests 
+
+app.get('/scrape', function (req, res) {
+
+  //All the web scraping magic will happen here
+
+});
+
+app.listen('8081');
+
+console.log('Magic happens on port 8081');
+
+exports = module.exports = app;
