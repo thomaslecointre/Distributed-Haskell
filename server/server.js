c17784fdb14b9a061d930b50f3a35a998f9c9d5f
Thomas Lecointre <thomas.lecointre1@gmail.com>
2017-05-10 13:47:34 +0200

    Server with express

--- server/server.js
+++ server/server.js
@@ -1,21 +1,23 @@
 // Imports
 var express = require("express");
 var fs      = require("fs");
-var request = require("request");
-var cheerio = require("cheerio");
-var app     = express();
 var imdb    = require("imdb-api");
+var app     = express();
 
-// Transform client requests 
 
-app.get('/scrape', function (req, res) {
-
-  //All the web scraping magic will happen here
-
+app.get('/', function (request, res) {
+    response = {
+        title : "Game of Thrones",
+        description : "L'histoire de la série, située sur les continents fictifs de Westeros et Essos à la fin d'un été d'une dizaine d'années, entrelace plusieurs intrigues. La première intrigue suit l'histoire des membres de plusieurs familles nobles, dans une guerre civile pour conquérir le Trône de Fer du Royaume des Sept Couronnes. La deuxième intrigue couvre l'histoire de Jon Snow et de la future menace croissante de l'hiver approchant, des créatures mythiques et légendaires venues du Nord du Mur de Westeros. La troisième raconte la démarche de Daenerys Targaryen au sud d'Essos, la dernière représentante en exil de la dynastie déchue en vue de reprendre le Trône de Fer. À travers ces personnages « moralement ambigus », la série explore les sujets liés au pouvoir politique, à la hiérarchie sociale, la religion, la guerre civile, la sexualité, et à la violence en général."
+    };
+    console.log(response);
+    res.end(JSON.stringify(response));
 });
 
-app.listen('8081');
 
-console.log('Magic happens on port 8081');
+var server = app.listen(8081, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log("Haskell-Search is listening at http://%s:%s", host, port);
+});
 
-exports = module.exports = app;
