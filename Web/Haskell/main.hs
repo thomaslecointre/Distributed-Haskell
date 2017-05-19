e42fbfc889bc05502dc18738639ccd1b21b5b073
Thomas Lecointre <thomas.lecointre1@gmail.com>
2017-05-19 16:00:12 +0200

    Parsing arguments

--- Web/Haskell/main.hs
+++ Web/Haskell/main.hs
@@ -1,13 +1,25 @@
 import System.Environment
 import Data.String
+import Network.HTTP.Client
+import Network.HTTP.Types.Status (statusCode)
 
 main :: IO()
 main = do
-  print args
-  url <- buildUrl getArgs
-  -- print url
-  print "hello"
+  args <- getArgs
+  putStrLn $ args !! 0
+  let url = buildUrl args
+  print url
 
-buildUrl :: IO [String] -> String
+  manager <- newManager defaultManagerSettings
+
+  request <- parseRequest url
+  response <- httpLbs request manager
+
+  putStrLn $ "The status code was: " ++ (show $ statusCode $ responseStatus response)
+  print $ responseBody response
+
+
+
+buildUrl :: [String] -> String
 buildUrl [""]   = ""
-buildUrl args = "http://localhost:8081/" ++ (args !! 0) ++ "/" ++ "1" ++ "/" ++ (args !! 1)
+buildUrl a = "http://localhost:8081/Public/" ++ a !! 0 ++ "/1/" ++ a !! 1
