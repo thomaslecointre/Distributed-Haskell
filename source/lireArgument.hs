c4193d5972cd7bff6c3b0a32c629d9be47bf439c
Thomas Perles <thomas.perles@gmail.com>
2017-05-22 09:59:40 +0200

    read arguments given to the slave
    [IP, Port, "Ned, "GoT","1","10"] -> ("Ned", IP:Port"/GoT/1/10")

--- source/lireArgument.hs
+++ source/lireArgument.hs
@@ -1,42 +1,20 @@
--- import Data.List
 import System.Environment
 
-firstList :: [String] -> String
-firstList [] = ""
-firstList (x:l) = x
+parseIpPortKeywordSerieSeasonEpisode :: [String] -> (String, String)
+parseIpPortKeywordSerieSeasonEpisode [] = ("","")
+parseIpPortKeywordSerieSeasonEpisode (ip:port:keyword:serie:season:episode:[]) = (keyword, ip ++ ":" ++ port ++ "/" ++ serie ++ "/" ++ season ++ "/" ++ episode)
 
-tailList :: [String] -> [String]
-tailList [] = []
-tailList (x:l) = l
+firstPair :: (String, String) -> String
+firstPair (a,b) = a
 
-episodesList :: Int -> [String] -> [(Int,String)]
-episodesList 0 _ = []
-episodesList _ [] = []
-episodesList n (x:l) = (n - length l, x):(episodesList n l)
-
-listStringNumberToList :: String -> [Int]
-listStringNumberToList n = [1..(read n :: Int)]
-
-listSeasonEpisode :: Int -> [Int] -> [(String,String)]
-listSeasonEpisode _ [] = []
-listSeasonEpisode season (x:l) = (show season, show x):(listSeasonEpisode season l)
-
-listAllSeasonEpisode :: [(Int, String)] -> [(String, String)]
-listAllSeasonEpisode [] = []
-listAllSeasonEpisode ((season,nbEpisodes):l) = (listSeasonEpisode season (listStringNumberToList nbEpisodes))++(listAllSeasonEpisode l)
-
-listTotale :: [String] -> [(String, String)]
-listTotale s = listAllSeasonEpisode (episodesList (length s) s)
-
-listURL :: String -> [(String,String)] -> [String]
-listURL _ [] = []
-listURL serie ((season,episode):l) = (serie ++ "/" ++ season ++ "/" ++ episode):(listURL serie l)
+secondPair :: (String, String) -> String
+secondPair (a,b) = b
 
 
-main = do
-    args <- getArgs -- ne marche pas -- ["GoT","3","3","2"]
-    let serie = firstList args -- "GoT"
-    let episodes = tailList args -- ["3","3","2"]
-    let tmp = listTotale episodes -- [("1","1"),("1","2"),("1","3"),("2","1"),("2","2"),("2","3"),("3","1"),("3","2")]
-    let res = listURL serie tmp -- ["GoT/1/1","GoT/1/2","GoT/1/3","GoT/2/1","GoT/2/2","GoT/2/3","GoT/3/1","GoT/3/2"]
-    print res
+main = do -- ne marche pas 
+    args <- getArgs -- [IP, Port, "Ned, "GoT","1","10"]
+    let keywordURL = parseIpPortKeywordSerieSeasonEpisode args -- ("Ned", IP:Port"/GoT/1/10")
+    print keywordURL
+    print $ "\n keyword : " ++ (firstPair keywordURL) -- "Ned"
+    print $ "\n URL : " ++ (secondPair keywordURL) -- "IP:Port"/GoT/1/10"
+    
