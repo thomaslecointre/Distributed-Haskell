f65aae50d0ae8046e4a547b8682cbec98bbd7bb9
Thomas Lecointre <thomas.lecointre1@gmail.com>
2017-05-26 16:33:33 +0200

    Registering IP on slave

--- test/slave-test.hs
+++ test/slave-test.hs
@@ -1,3 +1,4 @@
+import Network
 import Network.Info
 import System.IO
 import System.Environment
@@ -5,7 +6,16 @@ import System.Environment
 
 main = do
     interfaces <- getNetworkInterfaces
-    mapM extractIP interfaces
+    -- mapM extractIP interfaces
+    connect
+
+connect :: IO ()
+connect = withSocketsDo $ do
+    let port = fromIntegral 4444
+    handle <- connectTo "192.168.1.13" (PortNumber port)
+    -- hPutStrLn handle $ show 123456789
+    hClose handle
+
 
 extractIP :: NetworkInterface -> IO ()
 extractIP interface = print $ "Network : " ++ (name interface) ++ " IP : " ++ show (ipv4 interface)
