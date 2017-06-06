ecbc7bb99ef839e75e490aad4586bd8ed5885180
Thomas Lecointre <thomas.lecointre1@outlook.fr>
2017-06-06 08:54:04 +0200

    fixing script

--- node.sh
+++ node.sh
@@ -1,13 +1,17 @@
 # Kill node process if running
 PID_NODE=`ps -ef | grep "node server.js" | head -1 | awk '{print $2}'`
 
-if [ ! -z $PID_NODE ] then kill $PID_NODE
+if [ ! -z $PID_NODE ]; 
+    then kill $PID_NODE
+fi
 
 # kill master process if running
 
 PID_MASTER=`ps -ef | grep "./master.exe" | head -1 | awk '{print $2}'`
 
-if [ ! -z $PID_MASTER ] then kill $PID_MASTER
+if [ ! -z $PID_MASTER ]; 
+    then kill $PID_MASTER
+fi
 
 # recompile master, messenger
 
