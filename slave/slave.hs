e5ed1d52d3b1718dbf6e36283e28fbb89e8a8875
Thomas Perles <thomas.perles@gmail.com>
2017-06-08 13:43:20 +0200

    slave compilation ok

--- slave/slave.hs
+++ slave/slave.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE DeriveGeneric #-}
+
 import qualified Network as N
 import qualified ArgumentsParser as AP
 import System.IO
@@ -10,6 +12,12 @@ import GHC.Generics (Generic)
 import System.Environment
 import Data.String
 import Network.HTTP
+import Data.Aeson (FromJSON, ToJSON, decode, encode)
+import qualified Data.Text as DT
+import qualified GetWordsFilev2 as GWF
+import qualified PorterStemmer as PS
+import qualified Statisticsv2 as Stat
+import qualified KeyWordOccurrence as KWO
 
 main :: IO ()
 main = do
@@ -44,7 +52,7 @@ executeOrder incomingOrder = do
  let urls = map extractURLs parsedArguments
  descriptions <- getAllContent urls
  let updatedContent = updateAllContent parsedArguments descriptions
- 
+ let tabtabSeasonEpisodeOccurrence = map processEachSeason updatedContent
  putStrLn ""
  {-
  handle <- N.connectTo "185.167.204.218" (N.PortNumber 4446)
@@ -85,4 +93,40 @@ updateContent [] [] = []
 updateContent ((a,b,c,url):xs) (body:ys) = (a, b, c, body) : (updateContent xs ys)
 
 
+data Serie = Serie { season :: Int, name :: String, episode :: Int, plot :: String }
+    deriving (Show, Generic)
+
+instance FromJSON Serie
+instance ToJSON Serie
+
+processEachSeason :: [(String, Int, Int, String)] -> [[Int]]
+processEachSeason [] = []
+processEachSeason (x:xs) = (processEachEpisode x) : (processEachSeason xs) -- [("jon",1,1,"jon"),("jon",1,1,"jon jon"),("jon",1,1,"jon jon jon")]
+
+processEachEpisode :: (String, Int, Int, String) -> [Int] 
+processEachEpisode (keyword, season, episode, body) = do
+    let req = decode $ BL.pack body :: Maybe Serie
+    case req :: Maybe Serie of
+        Nothing -> do
+                --putStrLn "Couldn't load the JSON data from decode Aeson"
+                []
+        Just req -> do
+                    stemmingProcess (keyword, season, episode, (show.plot) req)
+
+{-
+    Entree :
+        - keyword
+        - season
+        - episode
+        - text
+        ex : ("jon", 1, 1, "Jon the Hand of the King ... an army")
+    Sortie :
+        [Int] (season, episode, occurrence)
+-}
+stemmingProcess :: (String, Int, Int, String) -> [Int]
+stemmingProcess (keyword, season, episode, text) = --let (keyword, season, episode, req) = ("jon", 1, 1, "Jon the Hand of the King ... an army") in
+    let gwf = GWF.getWordsFile text in
+    let kw = (DT.unpack . DT.toLower . DT.pack) (lines (PS.porterStemmer keyword) !! 0) in -- root of the keyword
+    let ps = DT.unpack . DT.toLower . DT.pack $ PS.porterStemmer gwf in -- "jon\nhand\nking\n...\narmi\n"
+    [season,episode,KWO.keyWordOccurrence kw ps]
 
