14dcf05620f14faaeb15fb42f4be770b9f6e8eb6
U-UHA\e1502420 <e1502420@athena.uha.fr>
2017-06-08 13:30:52 +0200

    IO insanity

--- slave/slave.hs
+++ slave/slave.hs
@@ -1,6 +1,5 @@
 import qualified Network as N
 import qualified ArgumentsParser as AP
-import qualified Network.Download as ND
 import System.IO
 import System.Environment
 import Control.Concurrent
@@ -10,8 +9,7 @@ import qualified Data.ByteString.Lazy.Char8 as BL
 import GHC.Generics (Generic)
 import System.Environment
 import Data.String
-import Network.HTTP.Client
-import Network.HTTP.Types.Status (statusCode)
+import Network.HTTP
 
 main :: IO ()
 main = do
@@ -40,21 +38,20 @@ receiveOrder handle incomingOrder = do
 -- | Reads and executes next order
 executeOrder :: Chan [[String]] -> IO ()
 executeOrder incomingOrder = do
-    order <- readChan incomingOrder
-    -- let work = process order -- !!!
-    let parsedArguments = map AP.parseArguments order
-    let urls = map extractURLs parsedArguments
-    descriptions <- (mapM . mapM) ( \url -> do
-                                    manager <- newManager defaultManagerSettings
-                                    request <- parseRequest url
-                                    response <- httpLbs request manager
-                                    return $ BL.unpack $ responseBody response ) urls
-    
-    -- ...
-    handle <- N.connectTo "185.167.204.218" (N.PortNumber 4446)
-    hSetBuffering handle LineBuffering
-    -- hPutStrLn handle work
-    hClose handle
+ order <- readChan incomingOrder
+
+ let parsedArguments = map AP.parseArguments order
+ let urls = map extractURLs parsedArguments
+ descriptions <- getAllContent urls
+ let updatedContent = updateAllContent parsedArguments descriptions
+ 
+ putStrLn ""
+ {-
+ handle <- N.connectTo "185.167.204.218" (N.PortNumber 4446)
+ hSetBuffering handle LineBuffering
+ -- hPutStrLn handle work
+ hClose handle
+ -}
 
 extractURLs :: [(String, Int, Int, String)] -> [String]
 extractURLs [] = []
@@ -62,3 +59,30 @@ extractURLs (x:xs) = (getURL x) : (extractURLs xs)
 
 getURL :: (String, Int, Int, String) -> String
 getURL (_,_,_,u) = u
+
+get 	:: String -> IO String
+get url = simpleHTTP (getRequest url) >>= getResponseBody
+
+getAllContent 			:: [[String]] -> IO [[String]]
+getAllContent (s:sx)  	= do
+ c <- getContent s
+ cx <- getAllContent sx
+ return (c:cx)
+
+getContent 			:: [String] -> IO [String]
+getContent (e:ex) = do
+ c <- get e
+ cx <- getContent ex
+ return (c:cx)
+
+updateAllContent :: [[(String, Int, Int, String)]] -> [[String]] -> [[(String, Int, Int, String)]]
+updateAllContent [] _ = []
+updateAllContent (x:xs) (y:ys) = do
+ (updateContent x y) : (updateAllContent xs ys)
+
+updateContent ::  [(String, Int, Int, String)] -> [String] -> [(String, Int, Int, String)]
+updateContent [] [] = []
+updateContent ((a,b,c,url):xs) (body:ys) = (a, b, c, body) : (updateContent xs ys)
+
+
+
