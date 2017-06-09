44328881a9b9361a849e4568156caa702e21ae00
Thomas Lecointre <thomas.lecointre1@gmail.com>
2017-06-09 17:47:37 +0200

    Pathing

--- web/haskell/Messenger.hs
+++ web/haskell/Messenger.hs
@@ -2,14 +2,28 @@ import Network
 import System.Exit
 import System.IO
 import System.Environment
+import System.Directory
 import Numeric
 import Control.Concurrent
 import Data.String
 
 main = do
     xs <- getArgs
-    sendOrder 4445 xs
-	
+    let seasonName = xs !! 1
+    let path = "../public/" ++ seasonName
+    seasons <- listDirectory path
+    files <- discoverFiles seasons
+    let arguments = map (show . length) files
+    sendOrder 4445 (xs ++ arguments)
+
+
+discoverFiles :: [FilePath] -> IO [[FilePath]]
+discoverFiles seasons = do
+    mapM listDirectory seasons
+
+
+
+
 {-|
     Sends the order received from web server to master haskell process.
 -}
--- web/imdb.js
+++ web/imdb.js
@@ -27,43 +27,33 @@ module.exports = {
 
             imdb.getById(item.imdbid, {apiKey: '46e82526'}, (err, idThings) => {
 			
-			if(!idThings || err) {
-				console.log('An error has occurred. Please try again.');
-				res.end('An error has occurred. Please try again.');
-			} else {
+              if(!idThings || err) {
+                console.log('An error has occurred. Please try again.');
+                res.end('An error has occurred. Please try again.');
+              } else {
 
-              var season = idThings.season;
-              var episode = idThings.episode;
+                var season = idThings.season;
+                var episode = idThings.episode;
 
-              if(!fs.existsSync(path.join(seriesPath, season))) {
-                fs.mkdirSync(path.join(seriesPath, season));
+                if(!fs.existsSync(path.join(seriesPath, season))) {
+                  fs.mkdirSync(path.join(seriesPath, season));
+                }
+
+                if(!fs.existsSync(path.join(seriesPath, season, episode))) {
+                  fs.mkdirSync(path.join(seriesPath, season, episode));
+                }
+
+                var episodePath = path.join(seriesPath, season, episode, 'episode.json');
+                if(!fs.existsSync(episodePath)) {
+                  fs.appendFileSync(episodePath, JSON.stringify(idThings), { flag : 'a+' });
+                }
               }
-
-              if(!fs.existsSync(path.join(seriesPath, season, episode))) {
-                fs.mkdirSync(path.join(seriesPath, season, episode));
-              }
-
-              var episodePath = path.join(seriesPath, season, episode, 'episode.json');
-              if(!fs.existsSync(episodePath)) {
-                fs.appendFileSync(episodePath, JSON.stringify(idThings), { flag : 'a+' });
-              }
-			}
-			
             });
-            
-
           });
+
           var haskell = require('./haskell');
           var args = [keyword, urlTitle];
-          var items = fs.readdirSync(seriesPath);
-          for(var i = 0; i < items.length; i++) {
-            var seasonPath = path.join(seriesPath, (i + 1).toString());
-            var moreItems = fs.readdirSync(seasonPath);
-            args.push(moreItems.length);
-          }
-
           console.log("Arguments sent to messenger : " + args);
-
           haskell.launch(args);
         });
       }
