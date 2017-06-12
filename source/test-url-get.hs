9e8364d3d965f9e870ffb02589a0cb301d48b4f5
Thomas Lecointre <thomas.lecointre1@gmail.com>
2017-06-12 13:39:29 +0200

    Cleaning directories

--- source/test-url-get.hs
+++
@@ -1,9 +0,0 @@
-import Network.Download
-
-
-getBody :: String -> IO ()
-getBody url = do
-    doc <- openURIString url
-    case doc :: Either String String of
-        Left doc -> print doc
-        Right doc -> print doc 
--- testv1/json/readFile2.hs
+++
@@ -1,9 +0,0 @@
--- http://stackoverflow.com/questions/7867723/haskell-file-reading
-import System.IO  
-import Control.Monad
-
-
-main = do  
-        main = do  
-        contents <- readFile "readFileTest.txt"
-        print . map unwords $ map words $ lines contents
